925a824a9d226dbdf1a87d9b7e0a508faf887641
JessBarbosa27 <barbosajess27@gmail.com>
2025-01-04 23:12:28 +0000

    setup: initial commit

---
+++ src/main/java/com/jessbarbosa/apps/pawkit_patient_service/PawkitPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jessbarbosa.apps.pawkit_patient_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawkitPatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawkitPatientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jessbarbosa/apps/pawkit_patient_service/PawkitPatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessbarbosa.apps.pawkit_patient_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawkitPatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
